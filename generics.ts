52b9d47627877f543a21969e07ddac4a14b79814
Hayan Khan <hayankhanmehsood@gmail.com>
2024-08-04 00:05:42 +0500

    ...

--- generics.ts
+++ generics.ts
@@ -64,3 +64,85 @@ function detectType(val: string | number) {
 function provideId(id:string | null){
 !id?console.log('no id'):console.log(id)
 }
+
+
+//Instance of and type predicates
+
+function logValue(x: Date|string){
+    if(x instanceof Date){  //used where new keyword can be potentially used
+        console.log(x.toUTCString())
+    }else{
+        console.log(x.toUpperCase())
+    }
+}
+
+
+type Fish = {swim:()=>void}
+type Bird = {fly:()=>void}
+
+function isFish(pet: Fish | Bird):pet is Fish{
+    return (pet as Fish).swim !== undefined
+}
+
+function getFood (pet: Fish | Bird){
+    (isFish(pet))?"fish food":"bird food"
+}
+
+
+
+//Discriminated Unions and Exhaustiveness checking
+
+interface Circle{
+    kind:'circle';
+    radius:number;
+    
+}
+
+interface Square{
+    kind:'square';
+    side:number;
+}
+
+interface Rectangle{
+    kind:'rectangle';
+    width:number;
+    height:number;
+}
+
+
+
+type Shape=Circle|Square|Rectangle
+
+function getTrueShape(shape:Shape){
+    if(shape.kind==='circle'){
+        return Math.PI*shape.radius**2
+    }
+    // return shape.side * shape.side
+}
+
+
+//Exhaustiveness checking
+
+function getShape(shape:Shape){
+    switch(shape.kind){
+        case 'circle':return Math.PI*shape.radius**2
+        case 'square':return shape.side * shape.side
+        case 'rectangle':return shape.width * shape.height
+        default: const _defaultForShape:never=shape
+        return _defaultForShape
+    }
+}
+
+
+
+
+
+
+
+
+
+const log=(shape:Circle|Square|Rectangle)=>{
+    return shape
+}
+
+console.log(log({kind:'circle',radius:3}))
