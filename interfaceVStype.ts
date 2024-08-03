ff863636f8ead507f2d72af80a112c327c3b572f
Hayan Khan <hayankhanmehsood@gmail.com>
2024-08-03 16:43:40 +0500

    .

--- interfaceVStype.ts
+++ interfaceVStype.ts
@@ -48,3 +48,18 @@ let admin:Admin={
 
     let user1=new User1("dcaca",1);
     // console.log(user1.email) //this will give an error because email is private
+
+
+//another professional way of defining class in typescript
+
+    class User2{
+        readonly city:string; //this variable can only be read and not written to
+
+        constructor(private email:string,public name:string){ } //this is a shorthand way of defining the variables in the constructor and putting them in the class
+    }
+
+    const hhh= new User2("dcaca","Hayan");
+
+    console.log(hhh.city)
+    // console.log(hhh.email) //this will give an error because email is private
+    console.log(hhh.name)
